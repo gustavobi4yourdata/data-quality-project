6e54b9f6ee1a82ba4d191d5af166bb6fc87f9f78
Gustavo <gustavo.souza@bi4yourdata.com>
2024-09-06 18:16:55 -0300

    Upload all files

---
+++ src/contrato.py
@@ -0,0 +1,22 @@
+import pandera as pa
+from pandera.typing import Series 
+
+
+class MetricasFinanceirasBase(pa.DataFrameModel):
+    setor_da_empresa: Series[str]
+    receita_operacional: Series[float] = pa.Field(ge=0)
+    data: Series[pa.DateTime] 
+    percentual_de_imposto: Series[float] = pa.Field(in_range= {"min_value": 0, "max_value": 1})
+    custos_operacionais: Series[float] = pa.Field(ge=0)
+
+    class Config:
+        strict = True
+        coerce = True
+
+    @pa.check(
+            "setor_da_empresa", 
+            name = "Checagem código dos setores",
+            error = "Cógido do setor da empresa é inválido")
+    def checa_codigo_setor(cls, codigo: Series[str]) -> Series[bool]:
+        return codigo.str[:4].isin(['REP_', 'MNT_', 'VND_'])
+
---
+++ src/etl.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import pandera as pa
+
+
+def extrai_dados(dir_arquivos: str) -> pd.DataFrame:
+    pass
+
+def transforma_dados(df: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+def carrega_dados(df: pd.DataFrame) -> None:
+    pass
+ 
